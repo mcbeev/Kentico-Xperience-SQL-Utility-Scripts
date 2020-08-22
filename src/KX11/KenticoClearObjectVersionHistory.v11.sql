614da1606a2fbd87d9c7d9b6bf9575cf129bc0e1
Brian McKeiver <bmckeiver@bizstream.com>
2020-08-22 15:55:31 -0400

    Adding a few more clean up scripts

---
+++ src/KX11/KenticoClearObjectVersionHistory.v11.sql
@@ -0,0 +1,17 @@
+/* KenticoClearObjectVersionHistory.v11.sql		*/
+/* Goal: Clean up data from objects				*/
+/* Description: Truncates all version history   */
+/*  that can bloat a database. Be very careful	*/
+/*  with this one, there is no coming back		*/
+/* Intended Kentico Verison: 11.x               */
+/* Author: Brian McKeiver (mcbeev@gmail.com)    */
+/* Revision: 1.0                                */
+/* Take a backup first! Don't be THAT guy!      */
+
+UPDATE CMS_ObjectSettings
+	SET ObjectCheckedOutVersionHistoryID = NULL
+WHERE ObjectCheckedOutVersionHistoryID IS NOT NULL
+
+GO
+
+DELETE FROM CMS_ObjectVersionHistory
---
+++ src/KX11/KenticoDeleteStagingData.v11.sql
@@ -0,0 +1,15 @@
+/* KenticoDeleteStagingDAta.v11.sql				*/
+/* Goal: Clean up data from Content Staging		*/
+/* Description: Truncates all Staging tasks		*/
+/*  that can bloat a database. Be very careful	*/
+/*  with this one, there is no coming back		*/
+/* Intended Kentico Verison: 11.x               */
+/* Author: Brian McKeiver (mcbeev@gmail.com)    */
+/* Revision: 1.0                                */
+/* Take a backup first! Don't be THAT guy!      */
+
+TRUNCATE TABLE Staging_Synchronization
+
+GO
+
+DELETE FROM Staging_Task
